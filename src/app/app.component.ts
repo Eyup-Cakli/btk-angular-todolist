36a3eba931e98867ae1a0925de8db6ae9f84d8e4
Eyup Cakli <eyupcakli@hotmail.com>
2024-05-04 22:02:47 +0300

    feat: delete and filter fonctions added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatIconModule,
     MatButtonModule,
     FormsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -25,30 +25,49 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 export class AppComponent {
   title = 'todo-list';
   taskList: TaskModel[] = [];
-  newTask: TaskModel = {name: "", status: false};
+  newTask: TaskModel = { name: '', status: false };
   filteredData: TaskModel[] = [];
 
   addTask() {
-      this.taskList.push({ ...this.newTask });
-      this.filteredData.push({ ...this.newTask })
-      this.newTask.name = '';
+    this.taskList.push({ ...this.newTask });
+    this.filteredData.push({ ...this.newTask });
+    this.newTask.name = '';
   }
 
-  deleteTask(index: number){
-    this.taskList.splice(index,1);
+  deleteTask(index: number) {
+    this.taskList.splice(index, 1);
   }
 
   statusChange(task: TaskModel) {
     task.status = !task.status;
   }
 
-  deleteDoneTasks(){
-    const filteredData = this.taskList.filter(data => !data.status);
-    console.log(filteredData);
+  deleteDoneTasks() {
+    this.taskList = this.taskList.filter((task) => !task.status);
+  }
+  deleteAllTasks() {
+    this.taskList = [];
+  }
+
+  getStatusDone() {
+    const statusDoneData = this.taskList.filter((data) => data.status);
+    this.taskList = statusDoneData;
+    console.log(statusDoneData);
+  }
+
+  getStatusNotDone() {
+    const statusNotDoneData = this.taskList.filter((data) => !data.status);
+    this.taskList = statusNotDoneData;
+  }
+
+  getAll() {
+    const tasks = this.taskList.filter((data) => data);
+    this.taskList = tasks;
+    console.log(tasks);
   }
 }
 
-export interface TaskModel{
+export interface TaskModel {
   name: string;
   status: Boolean;
 }
